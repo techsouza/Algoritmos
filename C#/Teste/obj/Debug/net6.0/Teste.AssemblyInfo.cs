9f496e66a0cd126d8b37e16ebc3d0914c9e1d4c4
Willian Souza <willian9211@gmail.com>
2024-10-19 23:13:51 -0400

    teste

--- C#/Teste/obj/Debug/net6.0/Teste.AssemblyInfo.cs
+++ C#/Teste/obj/Debug/net6.0/Teste.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Teste")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c31b479ed1d5d04aa838bc4d6b300ff7aec461bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbf0f3fd2352a17acb024b02d824a3f770b36a33")]
 [assembly: System.Reflection.AssemblyProductAttribute("Teste")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Teste")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ C/URI/3046.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+   int a;
+   scanf("%d", &a);
+   a = ((a+1)*(a+2))/2;
+   printf("%d\n", a);
+   
+    return 0;
+}

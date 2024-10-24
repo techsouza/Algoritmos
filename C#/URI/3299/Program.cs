674356ee0120418cbf91fa30f79d5a30b7c62e77
Willian Souza <willian9211@gmail.com>
2024-10-24 15:25:03 -0400

    24/10

---
+++ C#/URI/3299/Program.cs
@@ -0,0 +1,10 @@
+﻿ string num = Convert.ToString(Console.ReadLine());
+
+            if (num.Contains("13")) {
+
+                Console.WriteLine("{0} es de Mala Suerte",num);
+            }
+            else {
+
+                Console.WriteLine("{0} NO es de Mala Suerte",num);
+            }
